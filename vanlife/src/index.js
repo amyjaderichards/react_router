c6d9147dcef64dcf4aaef8a79adbf635eecfec56
amyjaderichards <amyjaderichards@googlemail.com>
2023-02-27 09:20:00 +0000

    Setting up basic React Router components

--- vanlife/src/index.js
+++ vanlife/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 const App = () => {
@@ -8,4 +9,18 @@ const App = () => {
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const About = () => {
+  return (
+    <h1>About Page</h1>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+    <App />
+  </BrowserRouter>
+);
